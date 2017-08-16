7734da13500fc4d1138b9048c26a39bfc6f19833
jmtomczak <jakubmkt@gmail.com>
2017-08-16 16:53:12 +0200

    Unnecessary things removed

--- experiment.py
+++ experiment.py
@@ -34,7 +34,7 @@ parser.add_argument('--early_stopping_epochs', type=int, default=50, metavar='ES
                     help='number of epochs for early stopping')
 
 parser.add_argument('--warmup', type=int, default=100, metavar='WU',
-                    help='number of epochs for warm-up')
+                    help='number of epochs for warmu-up')
 
 # cuda
 parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -61,13 +61,13 @@ parser.add_argument('--pseudoinputs_std', type=float, default=0.01, metavar='PS'
                     help='std for init pseudo-inputs')
 
 # model: model name, prior
-parser.add_argument('--model_name', type=str, default='vae_2level', metavar='MN',
+parser.add_argument('--model_name', type=str, default='convvae_2level', metavar='MN',
                     help='model name: vae, vae_2level, convvae, convvae_2level')
 
 parser.add_argument('--prior', type=str, default='vampprior', metavar='P',
                     help='prior: standard, vampprior')
 
-parser.add_argument('--input_type', type=str, default='continuous', metavar='IT',
+parser.add_argument('--input_type', type=str, default='binary', metavar='IT',
                     help='type of the input: binary, gray, continuous')
 
 # experiment
@@ -133,7 +133,6 @@ def run(args, kwargs):
         model.cuda()
 
     optimizer = AdamNormGrad(model.parameters(), lr=args.lr)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     # ======================================================================================================================
     print(args)
